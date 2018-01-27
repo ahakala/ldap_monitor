d088a4ac8310c3241215dda1cd38f55b67b06710
Hakala <andyhakala@gmail.com>
2018-01-27 12:56:10 -0600

    updating to support multiple LDAP servers

--- checker.py
+++ checker.py
@@ -3,16 +3,15 @@ from ldap3 import Server, Connection, ALL
 from slackclient import SlackClient 
 
 with open("config.yml", 'r') as ymlfile:
-    cfg= yaml.load(ymlfile)
+    cfg = yaml.load(ymlfile)
 
 if cfg['slack']['enabled'] == True:
     channel = cfg['slack']['channel']
     token = cfg['slack']['key']
 
 logging.basicConfig(filename='mylog.log',level=logging.DEBUG)
-server = Server(cfg['ldap']['host'], connect_timeout=cfg['ldap']['timeout'])
-conn = Connection(server)
-
+servers = cfg['ldap']['host']
+timeout = connect_timeout=cfg['ldap']['timeout']
 
 def notify(message,channel,token):
 
@@ -25,14 +24,24 @@ def notify(message,channel,token):
     )
 
 
-try:
-    a = conn.bind()
-    if a == True:
-        logging.info('successful connection')
-except Exception as e:
-    logging.error(e)
-    if cfg['slack']['enabled'] == True:
-        message = "Unable ro connect to " + cfg['ldap']['host']+ ": " + str(e)
-        notify(message,channel,token)
+
+def check(server, timeout):
+    
+    server = Server(server, connect_timeout=timeout)
+    conn = Connection(server)
+    
+    try:
+        a = conn.bind()
+        if a == True:
+            logging.info('successful connection to %s' % server)
+    except Exception as e:
+        logging.error(e)
+        if cfg['slack']['enabled'] == True:
+            message = "Unable ro connect to %s : " % server + str(e) 
+            notify(message,channel,token)
 
 
+
+for server in servers:
+    
+    check(server, timeout)
